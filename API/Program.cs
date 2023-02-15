aa2a1ba65fa6103176af013c2edfc657604cbed9
Catherine <princess.corn@gmail.com>
2023-02-15 13:41:55 -0600

    Error Handling - Improving the swagger doc

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,9 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Core.Interfaces;
 using API.Middleware;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using API.Errors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +25,24 @@ builder.Services.AddDbContext<StoreContext>( opt =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.Configure<ApiBehaviorOptions>(options => 
+    {
+        options.InvalidModelStateResponseFactory = actionContext =>
+        {
+            var errors = actionContext.ModelState
+                            .Where(e => e.Value.Errors.Count > 0)
+                            .SelectMany(x => x.Value.Errors)
+                            .Select(x => x.ErrorMessage)
+                            .ToArray();
+
+            var errorResponse = new ApiValidationErrorResponse 
+            {
+                Errors = errors
+            };
+
+            return new BadRequestObjectResult(errorResponse);
+        };
+    });
 
 var app = builder.Build();
 
